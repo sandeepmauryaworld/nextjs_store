24c8cc54b46558f58f8f7d0b2fc293eab3f6ed58
unknown <mexicool541@gmail.com>
2023-01-19 13:12:52 +0530

    productListing and home page style

---
+++ components/productItem.js
@@ -0,0 +1,39 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link'
+import React from 'react'
+
+const ProductItem = ({product}) => {
+  return (
+    <div className='cart'>
+
+        <Link href={`/product/${product.slug}`}>
+
+        </Link>
+       
+        <img   
+        src={product.image}
+        alt={product.name}
+        className="rounded shadow"  />
+       
+        <div className="flex flex-col items-center justify-center p-5">
+
+        <Link href={`/product/${product.slug}`}>
+          
+            <h2 className="text-lg">{product.name}</h2>
+        
+        </Link>
+        </div>
+        <p className="mb-2">{product.brand}</p>
+        <p>₹{product.price}</p>
+        <button
+          className="primary-button"
+          type="button"
+        //   onClick={() => addToCartHandler(product)}
+        >
+          Add to cart
+        </button>
+    </div>
+  )
+}
+
+export default ProductItem
